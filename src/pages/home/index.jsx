c672f264a4dd0b947d8c66dc8ef420cf7f90fc7d
RodolfoJaques <rodolfojaques13@gmail.com>
2022-12-21 15:56:32 -0300

    feat: criação, estilização e responsividade dos componentes FormCreateProduct e FormEditProduct

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,14 +1,11 @@
 import HomeStl from "./styles";
 import Header from "../../components/Header";
-import ListCards from "../../components/ListCards";
 
 
 function Home () {
     return(
         <HomeStl>
           <Header />
-          <ListCards/>
-
         </HomeStl>
 
     )
